6ebe9d7eb60e5e71ad65175cbc7e8dff1131e288
YrSk-tech <32609324+YrSk-tech@users.noreply.github.com>
2021-12-06 08:55:21 +0200

    Updated 5 lab

--- Prim_algo.py
+++ Prim_algo.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Graph:
     def __init__(self, verticles):
         self.verticle = verticles
@@ -6,29 +9,44 @@ class Graph:
     def add_edge(self, a, b, c, d, e):
         self.graph.append([a, b, c, d, e])
 
-    def print_solution(self, graph, first_parametr, secound_parametr):
-        print(str(first_parametr) + "-" + str(secound_parametr) + ":" + str(graph[first_parametr][secound_parametr]))
+    def print_solution(self, parent):
+        for i in range(1, self.verticle):
+            print(parent[i], "-", i, "\t", self.graph[i][parent[i]])
 
-    def algo(self, selected_node):
-        INF = 9999999
-        no_edge = 0
-        selected_node[0] = True
-        while no_edge < self.verticle - 1:
 
-            minimum = INF
-            a = 0
-            b = 0
-            for m in range(self.verticle):
-                if selected_node[m]:
-                    for n in range(self.verticle):
-                        if (not selected_node[n]) and self.graph[m][n]:
-                            if minimum > self.graph[m][n]:
-                                minimum = self.graph[m][n]
-                                a = m
-                                b = n
-            self.print_solution(self.graph, a, b)
-            selected_node[b] = True
-            no_edge += 1
+    def minKey(self, key, mstSet):
+
+        min_value = sys.maxsize
+
+        for v in range(self.verticle):
+            if key[v] < min_value and mstSet[v] == False:
+                min_value = key[v]
+                min_index = v
+
+        return min_index
+
+    def primMST(self, node):
+
+        key = [sys.maxsize] * self.verticle
+        parent = [None] * self.verticle
+        key[node] = 0
+        mstSet = [False] * self.verticle
+
+        parent[node] = -1
+
+        for cout in range(self.verticle):
+
+            u = self.minKey(key, mstSet)
+
+            mstSet[u] = True
+
+            for v in range(self.verticle):
+                if self.graph[u][v] > 0 and mstSet[v] == False and key[v] > self.graph[u][v]:
+                    key[v] = self.graph[u][v]
+                    parent[v] = u
+
+        self.print_solution(parent)
+        return mstSet
 
 
 if __name__ == '__main__':
@@ -40,5 +58,5 @@ if __name__ == '__main__':
     g.add_edge(0, 9, 1, 0, 1)
     g.add_edge(0, 2, 6, 1, 0)
 
-    selected_node = [0, 0, 0, 0, 0]
-    print(g.algo(selected_node))
+    node = 0
+    g.primMST(node)
--- test.py
+++ test.py
@@ -8,14 +8,12 @@ class PrimTest(unittest.TestCase):
         self.g.add_edge(0, 2, 0, 6, 0)
         self.g.add_edge(2, 0, 3, 8, 5)
         self.g.add_edge(0, 3, 0, 0, 7)
-        self.g.add_edge(6, 8, 0, 0, 9)
-        self.g.add_edge(0, 5, 7, 9, 0)
+        self.g.add_edge(0, 0, 0, 0, 0)
+        self.g.add_edge(0, 0, 0, 0, 0)
 
     def test_prim(self):
-        self.assertEqual(self.g.algo(0), [0, 2, -1, -4, 5])
+        self.assertEqual(self.g.primMST(0), [True, True, True, True, True])
 
-    def test_prim_inf(self):
-        self.assertEqual(self.g.algo(1), [float("Infinity"), 0, -3, -6])
 
 if __name__ == '__main__':
     unittest.main()
