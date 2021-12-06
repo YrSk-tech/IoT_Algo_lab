cfa952dfee23eca0f53e4ec024ab1540f124b07d
YrSk-tech <32609324+YrSk-tech@users.noreply.github.com>
2021-12-06 11:24:49 +0200

    Reworked 5 lab

--- test.py
+++ test.py
@@ -5,11 +5,25 @@ from Prim_algo import Graph
 class PrimTest(unittest.TestCase):
     def setUp(self):
         self.g = Graph(5)
-        self.g.add_edge(0, 2, 0, 6, 0)
-        self.g.add_edge(2, 0, 3, 8, 5)
-        self.g.add_edge(0, 3, 0, 0, 7)
-        self.g.add_edge(0, 0, 0, 0, 0)
-        self.g.add_edge(0, 0, 0, 0, 0)
+        self.g.add_edge(0, 1, 17)
+        self.g.add_edge(0, 2, 37)
+        self.g.add_edge(0, 3, 657)
+        self.g.add_edge(0, 4, 345)
+        self.g.add_edge(1, 2, 345)
+        self.g.add_edge(1, 3, 7467)
+        self.g.add_edge(1, 4, 654)
+        self.g.add_edge(2, 3, 253)
+        self.g.add_edge(2, 4, 213)
+        self.g.add_edge(2, 1, 7865)
+        self.g.add_edge(3, 1, 123)
+        self.g.add_edge(3, 2, 457)
+        self.g.add_edge(3, 4, 6457)
+        self.g.add_edge(3, 0, 4567)
+        self.g.add_edge(4, 0, 4576)
+        self.g.add_edge(4, 1, 7456)
+        self.g.add_edge(4, 2, 7568)
+        self.g.add_edge(4, 3, 353)
+
 
     def test_prim(self):
         self.assertEqual(self.g.primMST(0), [True, True, True, True, True])
